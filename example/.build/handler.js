46b2dc16dee4245736032663376babc75cb8422a
Dennis Peters <denjpeters@gmail.com>
2020-08-12 11:10:09 -0400

    Offline working with cert

--- example/.build/handler.js
+++ example/.build/handler.js
@@ -1,24 +1,23 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
+var serverless = require('serverless-http');
+var express = require('express');
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', (req, res) => {
-    const stage = !!process.env.STAGE ? process.env.STAGE : 'local'
-    
+app.get('/', function (req, res) {
+    var stage = !!process.env.STAGE ? process.env.STAGE : 'local';
     res.send({ application: 'sample-app', version: '1.00', stage: stage });
 });
-app.get('/api/info', (req, res) => {
+app.get('/api/info', function (req, res) {
     res.send({ application: 'sample-app', version: '1.010', stage: process.env.STAGE, isOffline: process.env.IS_OFFLINE });
 });
-app.post('/api/v1/getback', (req, res) => {
+app.post('/api/v1/getback', function (req, res) {
     res.send({ application: 'sample-app', version: '1.02' });
     // res.send({ ...req.body });
 });
-app.get('/', (req, res) => {
-    const stage = !!process.env.STAGE ? process.env.STAGE : 'local'
-    
+app.get('/', function (req, res) {
+    var stage = !!process.env.STAGE ? process.env.STAGE : 'local';
     res.send({ application: 'sample-app', version: '0.00', stage: stage });
 });
 //app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
+//# sourceMappingURL=handler.js.map
