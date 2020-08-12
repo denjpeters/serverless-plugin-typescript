4de5da4e5e5aab1d465383e2973079138476c4f9
Dennis Peters <denjpeters@gmail.com>
2020-08-12 09:10:07 -0400

    Environment variables passed!

--- example/handler.ts
+++ example/handler.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send({ application: 'sample-app', version: '1.00' });
+    res.send({ application: 'sample-app', version: '1.00', stage: process.env.STAGE });
 });
 app.get('/api/info', (req, res) => {
     res.send({ application: 'sample-app', version: '1.01' });
