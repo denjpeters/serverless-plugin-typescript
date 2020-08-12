bcc1653a077e9974ef0a814d4fbe9dc68a298ca4
Dennis Peters <denjpeters@gmail.com>
2020-08-12 08:04:21 -0400

    Added domain

--- example/handler.ts
+++ example/handler.ts
@@ -10,7 +10,8 @@ app.get('/api/info', (req, res) => {
     res.send({ application: 'sample-app', version: '1.01' });
 });
 app.post('/api/v1/getback', (req, res) => {
-    res.send({ ...req.body });
+    res.send({ application: 'sample-app', version: '1.02' });
+    // res.send({ ...req.body });
 });
 //app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
